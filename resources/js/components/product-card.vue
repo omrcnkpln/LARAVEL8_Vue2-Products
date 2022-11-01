2ad904d97ba2f156f1ae7289b483b451cb06e332
OmerCanKAPLAN <kaplan_911@hotmail.com>
2022-11-02 00:08:08 +0300

    first commit

---
+++ resources/js/components/product-card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hello World !</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "product-card",
+    mounted() {
+        console.log('Example component mounted.')
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
